6160f5a5e30219746aac239fd804414327bdcf92
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2024-02-20 22:45:50 +0000

    feat: add options to show full table and bot name

--- README.md
+++ README.md
@@ -17,7 +17,30 @@ Example of parsing one of the map folder from the [official result of the 2023 c
 ```console
 $ git clone https://github.com/rubensolv/MicroRTS2023Competition.git
 $ cd MicroRTS2023Competition
+$ rm -rf ./FourBWorkers8x8/tournament_1  # since the it is not used in the official one.
 $ python -m microrts_trp ./FourBWorkers8x8
 Map: FourBWorkers8x8
-
+Number of AIs: 17
+| Bot                                     |   Win Rate |
+|-----------------------------------------|------------|
+| RAISocketAI                             |      90.00 |
+| mayari{AStarPathFinding}                |      82.94 |
+| ObiBotKenobi                            |      81.76 |
+| NaiveMCTS(100, -1, 100,10,0.3, 1.0, ... |      75.59 |
+| 2L                                      |      65.59 |
+| Aggrobot(AStarPathFinding)              |      62.06 |
+| MyMicroRtsBot(AStarPathFinding)         |      61.47 |
+| POWorkerRush(AStarPathFinding)          |      60.00 |
+| SaveTheBeesV4(AStarPathFinding)         |      51.76 |
+| bRHEAdBot                               |      50.00 |
+| myBot                                   |      38.24 |
+| POLightRush(AStarPathFinding)           |      34.71 |
+| sophia                                  |      32.94 |
+| RandomBiasedAI                          |      28.24 |
+| Ragnar                                  |      22.94 |
+| NIlSiBot(GreedyPathFinding)             |      12.35 |
+| Predator                                |       5.88 |
 ```
+
+Note that in the `competitionDataPreparation.xlsx`, the number used is `160`, but since there are 17 bots we
+are using `170`.
--- microrts_trp/__init__.py
+++ microrts_trp/__init__.py
@@ -12,12 +12,20 @@ from microrts_trp.tournament import (
     parse_map_folder,
 )
 
+def shorten_bot_name(name: str, max_length=35) -> str:
+    if len(name) > max_length:
+        return name[:max_length+1] + "..."
+    else:
+        return name
+
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("map_folder", metavar="map-folder", nargs="?", default=".")
+    parser.add_argument("--full", action="store_true")
+    parser.add_argument("--full-bot-name", action="store_true")
 
     args = parser.parse_args()
     map_result = parse_map_folder(args.map_folder)
@@ -26,22 +34,34 @@ def main():
     final_scores["Total"] = final_scores.sum(axis=1)
 
     win_rates = map_result.win_rates.copy()
-    win_rates["Total"] = (
+    win_rates["Win Rate"] = (
         final_scores["Total"]
         * 100.0
         / (len(map_result.tournaments[0].ai_names) * map_result.total_iterations)
     )
-    # Move Total column to the left
+    # Move 'Win Rate' column to the left.
     cols = win_rates.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     win_rates = win_rates.loc[:, cols]
 
-    print(f"\nMap: {map_result.name}")
+    win_rates = win_rates.sort_values(by="Win Rate", ascending=False)
+
+    if not args.full:
+        # Remove the detail columns.
+        cols = win_rates.columns.tolist()
+        win_rates = win_rates.drop(columns=cols[1:])
+
+    if not args.full_bot_name:
+        win_rates.index = win_rates.index.map(shorten_bot_name)
+
+    print(f"Map: {map_result.name}")
+    print(f"Number of AIs: {map_result.win_rates.shape[0]}")
+    print(f"Total battles / bot: {len(map_result.tournaments[0].ai_names) * map_result.total_iterations}")
     print(
         tabulate(
-            win_rates.sort_values(by="Total", ascending=False),
+            win_rates,
             tablefmt="github",
-            headers=["Bot"] + win_rates.columns.to_list(),
+            headers=["Bot"] + win_rates.columns.tolist(),
             floatfmt=".2f",
         )
     )
